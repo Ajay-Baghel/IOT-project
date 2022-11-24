803c6da06fea01ebe119a0783e8ebc43e7e89e0b
Shubham <shubhamg2205@gmail.com>
2022-11-24 18:59:47 +0530

    random changes

--- app.py
+++ app.py
@@ -8,11 +8,6 @@ import random
 from nmraNew import *
 import copy
 
-1) virtual anchor nodes jb bhi estimate krna h
-2) two nearest va's
-3) centroid calci which is init guess
-4) optimize using NMRA.
-
 class sensor:
     def __init__(self, type, x, y, x_e = 0, y_e = 0):
         self.type = type
@@ -56,16 +51,20 @@ for i in range(len(x_target)):
 
 # plotting graph for virtual and target nodes
 
-fig,ax=plt.subplots(figsize=(10, 10))
-
+fig,ax=plt.subplots()
 scatter=ax.scatter(x,y)
 # fig1,ax1=plt.subplots()
 ax.set_xlim([0,15])
 ax.set_ylim([0,15])
+cnt=0
+error=[]
 def update(itr):
+    global cnt
+    cnt=cnt+1
     global x_target
     global y_target,x,y
     changes=[]
+    target=[]
     # Adding random movement to the target nodes
     for i in range(len(x_target)):
         x_target[i]+=0.1*random.uniform(-15,15)
@@ -87,22 +86,31 @@ def update(itr):
         idx1,idx2=va_dist[0][1],va_dist[1][1]
         va1,va2=virtual_anchors[idx1],virtual_anchors[idx2]
         centeroid_x,centeroid_y=(va1[0]+va2[0]+anchor1.x)/3,(va1[1]+va2[1]+anchor1.y)/3
-        coordiantes=NMRA(centeroid_x-1,centeroid_x+1,centeroid_y-1,centeroid_y+1,10,3,30,[[anchor1.x,anchor1.y],va1,va2],[x_target[i],y_target[i]])
-        # print(int(coordiantes[0]),int(coordiantes[1]),int(x_target[i]),int(y_target[i]))
-        changes.append(math.dist([coordiantes[0],coordiantes[1]],[x_target[i],y_target[i]]))
-    # print("hello")   
+        coordiantes=NMRA(centeroid_x-1,centeroid_x+1,centeroid_y-1,centeroid_y+1,100,3,30,[[anchor1.x,anchor1.y],va1,va2],[x_target[i],y_target[i]])
+        changes.append(coordiantes)
+        target.append([x_target[i],y_target[i]])
+    # print("hello")
+    error.append([changes,target])   
     plt.cla()
     ax.set_xlim([0,15])
     ax.set_ylim([0,15])
-    ax.scatter([7.5],[7.5],c='forestgreen',label="Single Anchor Node",s=200)
-    ax.scatter(x,y,s=100,marker='D',c="chartreuse",label='Virtual Anchors')
-    ax.scatter(x_target,y_target,marker='2',s=200,label="Moving Target Nodes",c="deepskyblue")
-    ax.legend()
+    ax.scatter([7.5],[7.5],c='yellow')
+    ax.scatter(x,y)
+    ax.scatter(x_target,y_target,marker='^')
+    changes=np.array(changes)
+    xp,yp=changes.T
+    ax.scatter(xp,yp,marker="+")
     
-ani=FuncAnimation(fig=fig,func=update,interval=500)   
-plt.legend()
+    if cnt==5:
+        ani.event_source.stop()
+ani=FuncAnimation(fig=fig,func=update,interval=100)   
+
 plt.show()
-
-
-
-
+error_for_itr=[]
+print(error[0])
+for i in range(5):
+    tmp=[]
+    for j in range(5):
+        for k in range(5):
+            e=math.dist(error[i][j][k],error[i][j][k])
+            error_for_itr.append(e)
